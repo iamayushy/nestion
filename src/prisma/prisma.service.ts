3d41f34e4b4e163fd322a233e7dea7742949a855
Ayush yadav <ayushyadav@gofynd.com>
2023-11-11 20:23:36 +0530

    second bacnkend

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
-          // url: 'postgresql://postgres:postgres@localhost:5432/nest',
-        },
-      },
-    });
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // Note: this is optional
+    await this.$connect();
   }
 }
